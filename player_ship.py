fd29e98784c999c4529e8969493707448b0b34d5
Jan Šebele <sebelej2@students.zcu.cz>
2023-11-04 15:54:44 +0100

    přidání krátkého popisu v player_ship.py

--- player_ship.py
+++ player_ship.py
@@ -1,10 +1,13 @@
 import pygame
-import numpy as np
 from screensetup import ScreenSetup
 from ship import Ship
 
 
 class PlayerShip(Ship):
+    # Class for the player's ship.
+    # Constructor creates the ship based on parent class Ship and spawns it in the middle of the screen.
+    # The update() function calculates the angle between the ship and mouse positions and then calls the parent's class
+    # update (see Ship's update() function).
     def __init__(self):
         super().__init__("vlod.png", ScreenSetup.width/2, ScreenSetup.height/2)
 
