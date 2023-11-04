26525200b80ee2aa6c641c6a70a697a79ddbd0e1
Jan Šebele <sebelej2@students.zcu.cz>
2023-11-04 16:08:33 +0100

    přidání krátkého popisu v renderupdate.py a screensetup.py

--- renderupdate.py
+++ renderupdate.py
@@ -1,12 +1,15 @@
 import pygame
 
 
+# This file is used to store functions for rendering and updating.
 def render_background(screen):
-    background = pygame.image.load("space.png")
-    screen.blit(background, (0, 0))
+    # This function renders the background image.
+    screen.blit(pygame.image.load("space.png"), (0, 0))
 
 
 def update_groups(groups, screen):
+    # This function updates all the sprite groups in "groups" list. The order does matter, because the latter the group
+    # is in the list, the upper it will apper.
     for group in groups:
         group.draw(screen)
         group.update()
--- screensetup.py
+++ screensetup.py
@@ -2,13 +2,19 @@ import pygame
 
 
 class ScreenSetup:
-    fps = 60
+    # This class is used to store screen parameters and functions
+
+    # Screen parameters:
     width = 640
     height = 640
+    fps = 60
 
     def __init__(self):
+        # This is just there, so it can be a class :)
         pass
 
     @classmethod
     def start_setup(cls):
+        # This function "sets the screen on". The object does not have to be created, it can be called anytime (but it
+        # really has to be called only at the beginning of the code).
         return pygame.display.set_mode((cls.width, cls.height))
