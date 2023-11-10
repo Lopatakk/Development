7472c7c56298043781b2b44cbee66c780c1891cf
Tomáš Fyškus <t.fyskus@gmail.com>
2023-11-10 17:27:47 +0100

    Fullscreen, spawn 2 typu enemy

--- main.py
+++ main.py
@@ -1,103 +1,130 @@
-import random
 import pygame
 import sys
-import numpy as np
 import time
-from classes import Triangle, Projectile
+import random
+from player_ship import PlayerShip
+from screensetup import ScreenSetup
+from crosshair import Crosshair
+from projectile import Projectile
+from renderupdate import *
+from enemy import Enemy
+from checkbuttons import *
+from collisions import handle_collisions
+from enemy_spawn import EnemySpawner
+
 
 # general setup
 pygame.init()
 clock = pygame.time.Clock()
 
 # screen setup
-fps = 60
-screen_width = 640
-screen_height = 640
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = ScreenSetup.start_setup()
 
 # text
-font = pygame.font.Font('freesansbold.ttf', 32)
+font = pygame.font.Font('freesansbold.ttf', 30)
+
+
 
 while True:     # main loop
-    background = pygame.image.load("space.png")
-    pygame.mouse.set_visible(True)
-
-    # projectiles
+    # creating sprites/groups
+        # projectiles
     projectile_group = pygame.sprite.Group()
-
-    # triangle
-    triangle = Triangle("vlod.png")
-    triangle_group = pygame.sprite.Group()
-    triangle_group.add(triangle)
+        # player
+    player = PlayerShip()
+    player_group = pygame.sprite.Group()
+    player_group.add(player)
+        # enemy
+    enemy_group = pygame.sprite.Group()
+        # enemy spawn
+    zarovka_spawner = EnemySpawner(enemy_group, "zarovka", 5, 5, 35, 0.1, 1000, 100)  # Interval spawnování v sekundách
+    tank_spawner = EnemySpawner(enemy_group, "tank", 5, 30, 20, 0.1, 4500, 100)
+        # crosshair
+    crosshair = Crosshair()
+    crosshair_group = pygame.sprite.Group()
+    crosshair_group.add(crosshair)
 
     # rendering
         # background
-    screen.blit(background, (0, 0))
-        # projectiles
-    projectile_group.draw(screen)
-    projectile_group.update()
-        # triangle
-    triangle_group.draw(screen)
-    triangle_group.update()
+    render_background(screen)
+        # groups
+    update_groups([player_group, enemy_group], screen)
         # start text
     text_render = font.render("NEW GAME", True, (255, 255, 255))
-    screen.blit(text_render, (screen_width/2.7, screen_height/4))
-        # screen update
+    screen.blit(text_render, (ScreenSetup.width / 2, ScreenSetup.height / 2))
+
+    # screen update (must be at the end of the loop before waiting functions!)
     pygame.display.flip()
 
+
     # wait
-    time.sleep(5)
+    time.sleep(1)
 
     while True:     # game loop
-        for event in pygame.event.get():
-            # closing window
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
 
-        # death
-        if triangle.hp <= 0:
-            break
+        # Získání rozměrů obrazovky
+        pygame.init()
+        width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
 
-        # key/mouse pressing
-        key = pygame.key.get_pressed()
-        if key[pygame.K_w] == True:
-            triangle.velocity[1] += -1
-        if key[pygame.K_s] == True:
-            triangle.velocity[1] += +1
-        if key[pygame.K_a] == True:
-            triangle.velocity[0] += -1
-        if key[pygame.K_d] == True:
-            triangle.velocity[0] += +1
-        if key[pygame.K_SPACE] == True:
-            triangle.hp += -20
-        mouse = pygame.mouse.get_pressed(num_buttons=3)
-        if mouse[0]:
-            projectile = Projectile("projectile.png", triangle)
-            projectile_group.add(projectile)
+        # Nastavení režimu full screen
+        screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+        pygame.display.set_caption('Space shooter')
 
-        # rendering
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+            for event in pygame.event.get():
+                # closing window
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                # player death
+            if not player_group:
+                break
+
+                # key/mouse pressing
+                # WSAD
+            player.velocity += check_wsad()
+            # mouse
+            mouse = pygame.mouse.get_pressed(num_buttons=5)
+            if mouse[0]:
+                projectile_group.add(player.shoot())
+
+            # rendering/update
             # background
-        screen.blit(background, (0, 0))
-            # projectiles
-        projectile_group.draw(screen)
-        projectile_group.update()
-            # triangle
-        triangle_group.draw(screen)
-        triangle_group.update()
-            # screen update
+            render_background(screen)
+
+            # groups
+            update_groups([projectile_group, player_group, enemy_group, crosshair_group], screen)
+
+            # enemy spawn updates
+            zarovka_spawner.update(player.pos)
+            tank_spawner.update(player.pos)
+            # collisions
+            handle_collisions(enemy_group, player_group)
+            handle_collisions(projectile_group, enemy_group)
+            # handle_collisions(projectile_group, player_group)
+
+            # screen update (must be at the end of the loop before waiting functions!)
+            pygame.display.flip()
+
+            # FPS
+            clock.tick(ScreenSetup.fps)
+
+
+        # death text
+        crosshair.disable()
+        # EXIT text
+        font = pygame.font.Font(None, 36)
+        exit_text = font.render("SMRT", True, (255, 255, 255))
+        screen.blit(exit_text, (width/2, height/2))
         pygame.display.flip()
-
-        print(triangle.velocity)
-
-        # FPS
-        clock.tick(fps)
-
-    # start text
-    text_render = font.render("SMRT", True, (255, 255, 255))
-    screen.blit(text_render, (screen_width / 2, screen_height / 2))
-    # screen update
-    pygame.display.flip()
-
-    # wait
-    time.sleep(5)
+        time.sleep(2)
---
+++ screensetup.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+class ScreenSetup:
+    # This class is used to store screen parameters and functions
+
+    # Screen parameters:
+    pygame.init()
+    width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+    fps = 60
+
+    def __init__(self):
+        # This is just there, so it can be a class :)
+        pass
+
+    @classmethod
+    def start_setup(cls):
+        # This function "sets the screen on". The object does not have to be created, it can be called anytime (but it
+        # really has to be called only at the beginning of the code).
+        return pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
