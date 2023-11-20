44e9bdff5ea279eaf2ff0b733ede88f22e9caa88
pvlfr <Paja.F320@seznam.cz>
2023-11-20 23:16:11 +0100

    Základ button pause menu (pořád pro vypnutí funguje "q")

--- pause_menu.py
+++ pause_menu.py
@@ -1,46 +1,49 @@
 import pygame
+import button
 from screensetup import ScreenSetup
 
 height = ScreenSetup.height  # finds height of screen
 width = ScreenSetup.width  # finds width of screen
-font = pygame.font.Font('assets/fonts/PublicPixel.ttf', 40)
-
+font_title = pygame.font.Font('assets/fonts/PublicPixel.ttf', 100)
+font_text = pygame.font.Font('assets/fonts/PublicPixel.ttf', 30)
+new_cursor = pygame.image.load("assets/images/crosshair.png")
 
 
 def pause_menu(screen, clock, score):
     game_paused = True
     pygame.mouse.set_visible(False)
-    new_cursor = pygame.image.load("assets/images/crosshair.png")
     new_cursor_rect = new_cursor.get_rect()
+
     surface = pygame.Surface(screen.get_size())
     surface = surface.convert_alpha()
+
+    # load button images
+    button_01_img = pygame.image.load('assets/images/button_01.png').convert_alpha()
+
+    # create button instances
+    resume_button = button.Button(4.2 * width / 20, 6 * height / 20, button_01_img, 1, 0.6)
+    main_menu_button = button.Button(4.2 * width / 20, 200, button_01_img, 1, 1)
+
 
     while game_paused:
         background_copy = screen.copy()
 
         surface.fill((0, 0, 0, 150))  # fill the whole screen with black transparent color
         screen.blit(surface, (0, 0))
+        screen.blit(font_title.render("Pause menu", True, (230, 230, 230)), (4.2 * width / 20, 3.4 * height / 20))
 
-        # creating rectangles for options
-        pause_menu_menu = pygame.draw.rect(screen, (128, 128, 128), [4*width/20, 3*height/20, 800, 100], 0, 10)
-        resume_menu = pygame.draw.rect(screen, (128, 128, 128), [4*width/20, 6*height/20, 500, 80], 0, 10)
-        main_menu_menu = pygame.draw.rect(screen, (128, 128, 128), [4*width/20, 8*height/20, 500, 80], 0, 10)
-        quit_menu = pygame.draw.rect(screen, (128, 128, 128), [4*width/20, 10*height/20, 500, 80], 0, 10)
-        score_menu = pygame.draw.rect(screen, (128, 128, 128), [4*width/20, 12*height/20, 500, 80], 0, 10)
 
-        # creating word for rectangles
-        screen.blit(font.render("Pause menu", True, "black"), (4.2*width/20, 3.4*height/20))
-        screen.blit(font.render("Resume: press ESC", True, "black"), (4.2*width/20, 6.3*height/20))
-        screen.blit(font.render("Main menu (neexistuje)", True, "black"), (4.2*width/20, 8.3*height/20))
-        screen.blit(font.render("Quit game: press Q", True, "black"), (4.2*width/20, 10.3*height/20))
-        score_text = "Score (neexistuje): " + str(score)
-        screen.blit(font.render(score_text, True, "black"), (4.2*width/20, 12.3*height/20))
+        if resume_button.draw(screen):
+            print('resume')
+        screen.blit(font_text.render("Resume", True, (50, 50, 50)), (4.2 * width / 20, 6 * height / 20))
+        # if main_menu_button.draw(screen):
+        #     print('main_menu')
 
         # closing pause  menu
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:     # to continue play
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:  # to continue play
                 return
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:          # to quit game
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_q:  # to quit game
                 quit()
             if event.type == pygame.QUIT:
                 quit()
