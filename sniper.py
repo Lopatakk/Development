4bee0bc5c32c530b4b4f4567763cae6e34fd582e
Jan Šebele <sebelej2@students.zcu.cz>
2023-11-27 11:30:48 +0100

    oprava nuloveho proj_dmg sniper.py

--- sniper.py
+++ sniper.py
@@ -7,7 +7,7 @@ from player_ship import PlayerShip
 
 class Sniper(Enemy):
     def __init__(self, start: np.ndarray, clock, projectile_group, player):
-        super().__init__(start, 5, "assets/images/zarovka.png", clock, 45, 0.1, 200, 150, 0.5, 0, projectile_group, 1, 1, 2, player)
+        super().__init__(start, 5, "assets/images/zarovka.png", clock, 45, 0.1, 200, 150, 0.5, 150, projectile_group, 1, 1, 2, player)
         self.image_non_rot = pygame.transform.scale(self.image_non_rot, (60, 100))
 
     def update(self):
