b0642496d6349b160da7f8e0f0c3cf2f9d856699
Jan Šebele <sebelej2@students.zcu.cz>
2023-12-28 16:21:35 +0100

    pridani strileci animace mida

--- playerships/playermid.py
+++ playerships/playermid.py
@@ -3,7 +3,7 @@ from playerships.blast import Blast
 from playership import PlayerShip
 from pygame.sprite import Group
 import json
-import numpy as np
+from projectile import Projectile
 
 
 class PlayerMid(PlayerShip):
@@ -21,7 +21,34 @@ class PlayerMid(PlayerShip):
         self.gun_upgrade_sound = pygame.mixer.Sound("assets/sounds/gun_upgrade.mp3")
         self.gun_upgrade_sound.set_volume(0.6)
 
+        # shooting animation
+        self.image_non_rot_orig = self.image_non_rot
+        self.shooting_images = []
+        for num in range(1, 6):
+            img = pygame.image.load(f"assets/animations/shooting/MID/MID{num}.png")
+            # add the image to the list
+            self.shooting_images.append(img)
+        self.index = 0
+        self.counter = -1
+        self.animation_speed = 1
+
     def update(self):
+        # shooting animation
+        if self.counter >= 0:
+            self.counter += 1
+        if self.counter >= self.animation_speed and self.index < len(self.shooting_images) - 1:
+            self.counter = 0
+            self.index += 1
+            if self.is_q_action_on:
+                self.index += 1
+            self.image_non_rot = self.shooting_images[self.index]
+        if self.index >= len(self.shooting_images) - 1 and self.counter >= self.animation_speed:
+            self.counter = -1
+            self.index = 0
+            self.image_non_rot = self.image_non_rot_orig
+            projectile = Projectile(self)
+            self.projectile_group.add(projectile)
+
         super().update()
 
     def q_action(self):
@@ -39,3 +66,16 @@ class PlayerMid(PlayerShip):
 
     def e_turn_off(self):
         pass
+
+    def shoot(self):
+        """
+        If the time after last shot is greater than fire_rate_time and the gun is not overheated, this function creates
+        (spawns) a projectile and adds it to the projectile group.
+        """
+        elapsed_time = self.time_alive - self.last_shot_time
+        if elapsed_time >= self.fire_rate_time and not self.is_overheated:
+            self.last_shot_time = self.time_alive
+            self.heat += 1
+
+            self.counter = 0
+            self.image_non_rot = self.shooting_images[self.index]
