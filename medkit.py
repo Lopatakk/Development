f145a5c7fde0a50a398465a75837fa5eb38ac1b9
Jan Šebele <sebelej2@students.zcu.cz>
2023-12-28 22:45:50 +0100

    pridani animace medkit.py

--- medkit.py
+++ medkit.py
@@ -11,6 +11,7 @@ class Medkit(pygame.sprite.Sprite):
 
         self.image = pygame.image.load("assets/images/medkit.png")
         self.image = pygame.Surface.convert_alpha(self.image)
+        self.image_orig = self.image
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.center = self.pos
@@ -20,5 +21,33 @@ class Medkit(pygame.sprite.Sprite):
         self.sound = pygame.mixer.Sound("assets/sounds/heal-up.mp3")
         self.sound.set_volume(0.2)
 
+        # animation
+        self.animation_images = []
+        for num in range(1, 7):
+            img = pygame.image.load(f"assets/animations/medkit/medkit{num}.png")
+            # add the image to the list
+            self.animation_images.append(img)
+        self.index = 0
+        self.counter = -1
+        self.animation_speed = 5
+        self.last_animation_time = 0
+        self.time_between_animations = 3
+
     def update(self):
+        # animation
+        elapsed_time = self.time_alive - self.last_animation_time
+        if elapsed_time >= self.time_between_animations:
+            self.counter = 0
+            self.last_animation_time = self.time_alive
+        if self.counter >= 0:
+            self.counter += 1
+        if self.counter >= self.animation_speed and self.index < len(self.animation_images) - 1:
+            self.counter = 0
+            self.index += 1
+            self.image = self.animation_images[self.index]
+        if self.index >= len(self.animation_images) - 1 and self.counter >= self.animation_speed:
+            self.counter = -1
+            self.index = 0
+            self.image = self.image_orig
+
         super().update()
