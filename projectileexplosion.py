37aad7fec2c0992bd0c08e44dffc644d9ea70f8c
Jan Šebele <sebelej2@students.zcu.cz>
2024-01-09 12:55:32 +0100

    scaling explozi, uprava spawnu projektilu

--- projectileexplosion.py
+++ projectileexplosion.py
@@ -1,21 +1,23 @@
 import pygame.sprite
+from screensetup import ScreenSetup
 
 
 class ProjectileExplosion(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         pygame.sprite.Sprite.__init__(self)
         self.images = []
+        self.screen_coefficient = ScreenSetup.width / 1920
         self.sound = pygame.mixer.Sound("assets/sounds/video_game_hit_sound.mp3")  # Load sound file
         self.sound.set_volume(0.05)
         pygame.mixer.find_channel(True).play(self.sound)
         for num in range(1, 8):
             img = pygame.image.load(f"assets/animations/projectile_collision1/proj_col{num}.png")
             if size == 1:
-                img = pygame.transform.scale(img, (150, 150))
+                img = pygame.transform.scale(img, (150 * self.screen_coefficient, 150 * self.screen_coefficient))
             if size == 2:
-                img = pygame.transform.scale(img, (300, 300))
+                img = pygame.transform.scale(img, (300 * self.screen_coefficient, 300 * self.screen_coefficient))
             if size == 3:
-                img = pygame.transform.scale(img, (500, 500))
+                img = pygame.transform.scale(img, (500 * self.screen_coefficient, 500 * self.screen_coefficient))
             # add the image to the list
             self.images.append(img)
         self.index = 0
